d3cacb7919f75af961638806fe2271cf69425721
rifki uchiha <kiiuciha@gmail.com>
2024-10-01 14:00:42 +0700

    first version

---
+++ src/utils/data.js
@@ -0,0 +1,54 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Fiony',
+            tag: 'jkt48.fiony',
+            imageUrl: '/public/images/fiony.png'
+        },
+        {
+            id: 2,
+            name: 'Chika',
+            tag: 'jkt48.chika',
+            imageUrl: '/public/images/chika.png'
+        },
+        {
+            id: 3,
+            name: 'Christy',
+            tag: 'jkt48.christy',
+            imageUrl: '/public/images/christy.png'
+        },
+        {
+            id: 4,
+            name: 'Freya',
+            tag: 'jkt48.freya',
+            imageUrl: '/public/images/freya.png'
+        },
+        {
+            id: 5,
+            name: 'Gita',
+            tag: 'jkt48.gita',
+            imageUrl: '/public/images/gita.png'
+        },
+        {
+            id: 6,
+            name: 'Indah',
+            tag: 'jkt48.indah',
+            imageUrl: '/public/images/indah.png'
+        },
+        {
+            id: 7,
+            name: 'Marsha',
+            tag: 'jkt48.marsha',
+            imageUrl: '/public/images/marsha.png'
+        },
+        {
+            id: 8,
+            name: 'Muthe',
+            tag: 'jkt48.muthe',
+            imageUrl: '/public/images/muthe.png'
+        }
+    ]
+}
+
+export { getData};
